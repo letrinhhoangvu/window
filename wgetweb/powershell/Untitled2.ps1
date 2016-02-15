cf772685aaa0056d80df2618a413e59f8e4274ad
vulth <letrinhhoangvu@gmail.com>
2016-02-15 15:16:42 +0700

    #f

---
+++ wgetweb/powershell/Untitled2.ps1
@@ -0,0 +1,3 @@
+﻿$FilesInFolder = Get-ChildItem  -Name -filter "*.html" D:\wgetweb\powershell\domino\leocdwiwf02.cloudapp.net+8080\imart
+echo $FilesInFolder | Out-File ..\vu.txt
+Compare-Object -ReferenceObject (Get-Content ..\vu.txt) -DifferenceObject  (Get-Content ..\vu2.txt) | WHERE {$_.SideIndicator -eq “<=”} | select -ExpandProperty InputObject | Out-File ..\vua.txt
